1a0b2da80408ac51afe7ce0ea498d96ca7ba3481
Roman Lobakov <lobakov@gmail.com>
2020-07-28 05:37:11 +0300

    Added minor formatting changes

--- src/main/java/ua/com/foxminded/anagram/AnagramApp.java
+++ src/main/java/ua/com/foxminded/anagram/AnagramApp.java
@@ -3,8 +3,7 @@ package ua.com.foxminded.anagram;
 public class AnagramApp {
 
     public static void main(String[] args) {
-        String line = "abcd efgh a1bcd efg!h  "
-                + "2eeeee4444eeewddffgttgtgt4567";
+        String line = "abcd efgh a1bcd efg!h  2eeeee4444eeewddffgttgtgt4567";
 
         AnagramCreator anagramCreator = new AnagramCreator();
 
